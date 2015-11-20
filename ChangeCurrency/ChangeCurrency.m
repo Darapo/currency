b5140742ea09f918b167c804339f578b757f1133
Korea Software HRD Center <koreasoftwarehrdcenter@naruto.local>
2015-11-20 14:26:13 +0700

    test

---
+++ ChangeCurrency/ChangeCurrency.m
@@ -0,0 +1,13 @@
+//
+//  ChangeCurrency.m
+//  ChangeCurrency
+//
+//  Created by Korea Software HRD Center on 11/20/15.
+//  Copyright © 2015 hrd. All rights reserved.
+//
+
+#import "ChangeCurrency.h"
+
+@implementation ChangeCurrency
+
+@end
