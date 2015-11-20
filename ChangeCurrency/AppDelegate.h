014b4a3edbb964efbd5d6bcedc0018425a9d2fe5
Korea Software HRD Center <koreasoftwarehrdcenter@naruto.local>
2015-11-20 14:13:37 +0700

    Initial Commit

---
+++ ChangeCurrency/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChangeCurrency
+//
+//  Created by Korea Software HRD Center on 11/20/15.
+//  Copyright © 2015 hrd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
